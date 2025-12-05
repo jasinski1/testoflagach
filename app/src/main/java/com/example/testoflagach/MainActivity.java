79cc94b3aebe73d0574f38900de5f6423ce6c078
jasinski1 <jasinskikuba41@gmail.com>
2025-12-05 14:14:29 +0100

    podsumowanie

--- app/src/main/java/com/example/testoflagach/MainActivity.java
+++ app/src/main/java/com/example/testoflagach/MainActivity.java
@@ -4,6 +4,7 @@ import static android.view.View.INVISIBLE;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -13,21 +14,30 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    int licznikprzyciskow;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        textView = findViewById(R.id.textView);
     }
 
     public void ukryj(View view) {
         view.setVisibility(INVISIBLE);
+        licznikprzyciskow++;
+        if(licznikprzyciskow == 4){
+           // Toast.makeText(this, "brawo wygrałes", Toast.LENGTH_SHORT).show();
+            textView.setText("barooooooooooooooooooooo flaga Polski");
+        }
     }
 
     public void ok(View view) {
